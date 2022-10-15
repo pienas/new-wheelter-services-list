9136050de1590cd72d08d6a03f3bd0bf4d6ef93f
Karolis Urbaitis <karolis.urbaitis@lyse.no>
2022-10-15 19:15:07 +0300

    Add icons

---
+++ src/components/icons/index.ts
@@ -0,0 +1,55 @@
+import { AddIcon } from './AddIcon/AddIcon';
+import { SearchIcon } from './SearchIcon/SearchIcon';
+import { MoreIcon } from './MoreIcon/MoreIcon';
+import { FilterIcon } from './FilterIcon/FilterIcon';
+import { EditIcon } from './EditIcon/EditIcon';
+import { RemoveIcon } from './RemoveIcon/RemoveIcon';
+import { AssignIcon } from './AssignIcon/AssignIcon';
+import { ArrowDownIcon } from './ArrowDownIcon/ArrowDownIcon';
+import { ArrowUpIcon } from './ArrowUpIcon/ArrowUpIcon';
+import { CheckIcon } from './CheckIcon/CheckIcon';
+import { CrossIcon } from './CrossIcon/CrossIcon';
+import { ChevronLeftIcon } from './ChevronLeftIcon/ChevronLeftIcon';
+import { ChevronRightIcon } from './ChevronRightIcon/ChevronRightIcon';
+import { ChevronDownIcon } from './ChevronDownIcon/ChevronDownIcon';
+import { PriorityIcon } from './PriorityIcon/PriorityIcon';
+import { DescriptionIcon } from './DescriptionIcon/DescriptionIcon';
+import { NameIcon } from './NameIcon/NameIcon';
+import { UrlIcon } from './UrlIcon/UrlIcon';
+import { EmailIcon } from './EmailIcon/EmailIcon';
+import { PhoneIcon } from './PhoneIcon/PhoneIcon';
+import { StatusIcon } from './StatusIcon/StatusIcon';
+import { NewCategoryIcon } from './NewCategoryIcon/NewCategoryIcon';
+import { SuccessIcon } from './SuccessIcon/SuccessIcon';
+import { ErrorIcon } from './ErrorIcon/ErrorIcon';
+import { InfoIcon } from './InfoIcon/InfoIcon';
+import { WarningIcon } from './WarningIcon/WarningIcon';
+
+export {
+  AddIcon,
+  MoreIcon,
+  SearchIcon,
+  FilterIcon,
+  EditIcon,
+  RemoveIcon,
+  AssignIcon,
+  ArrowDownIcon,
+  ArrowUpIcon,
+  CheckIcon,
+  CrossIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  ChevronDownIcon,
+  PriorityIcon,
+  DescriptionIcon,
+  NameIcon,
+  UrlIcon,
+  EmailIcon,
+  PhoneIcon,
+  StatusIcon,
+  NewCategoryIcon,
+  SuccessIcon,
+  ErrorIcon,
+  InfoIcon,
+  WarningIcon,
+};
