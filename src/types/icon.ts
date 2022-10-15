c4ee9900d7a4f87e49bfcf671a309d8024a56742
Karolis Urbaitis <karolis.urbaitis@lyse.no>
2022-10-15 19:18:26 +0300

    Add icons

---
+++ src/types/icon.ts
@@ -0,0 +1,11 @@
+export interface IconProps {
+  boxSize?: number;
+  color?: string;
+  mr?: string;
+  ml?: string;
+  mt?: string;
+  mb?: string;
+  m?: string;
+  my?: string;
+  mx?: string;
+}
